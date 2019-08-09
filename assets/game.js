7d0f42c3975a4ec3ee763ff88b6576077e2a3ada
Rosalis Freebairn <rfreeb@artic.edu>
2019-08-08 21:00:04 -0500

    chose a different effect and applied it to the h1 text instead of the title text tag. Effect not working fully. Psuedo code in javascript file.

--- assets/game.js
+++ assets/game.js
@@ -1,30 +1,72 @@
-// Wrap every letter in a span
-var textWrapper = document.querySelector(".ml12");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /([^\x00-\x80]|\w)/g,
-  "<span class='letter'>$&</span>"
-);
+// // Wrap every letter in a span
+// var textWrapper = document.querySelector(".ml6 .letters");
+// textWrapper.innerHTML = textWrapper.textContent.replace(
+//   /([^\x00-\x80]|\w)/g,
+//   "<span class='letter'>$&</span>"
+// );
 
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml12 .letter",
-    translateX: [40, 0],
-    translateZ: 0,
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 1200,
-    delay: function(el, i) {
-      return 500 + 30 * i;
-    }
-  })
-  .add({
-    targets: ".ml12 .letter",
-    translateX: [0, -30],
-    opacity: [1, 0],
-    easing: "easeInExpo",
-    duration: 1100,
-    delay: function(el, i) {
-      return 100 + 30 * i;
-    }
-  });
+// anime
+//   .timeline({ loop: true })
+//   .add({
+//     targets: ".ml6 .letter",
+//     translateY: ["1.1em", 0],
+//     translateZ: 0,
+//     duration: 750,
+//     delay: function(el, i) {
+//       return 50 * i;
+//     }
+//   })
+//   .add({
+//     targets: ".ml6",
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
+
+//  $(document).ready(function() {
+//         Here we are provided an initial array of letters.
+//         Use this array to set the alphabet that the computer and the player will guess.
+//         constters = [
+//           "A",
+//           "B",
+//           "C",
+//           "D",
+//           "E",
+//           "F",
+//           "G",
+//           "H",
+//           "I",
+//           "J",
+//           "K",
+//           "L",
+//           "M",
+//           "N",
+//           "O",
+//           "P",
+//           "Q",
+//           "R",
+//           "S",
+//           "T",
+//           "U",
+//           "V",
+//           "W",
+//           "X",
+//           "Y",
+//           "Z",
+//         ];
+
+// Creating an array of letters.
+//  const letters = ["A", "B", "C", "D"...];
+
+// Looping through each item in the array and logging a message to the console.
+
+//  for (let i = 0; i < letters.length; i++) {
+//    console.log(letters);
+//  }
+
+// Create the code that will prompt the computer to guess any of the 26 letters after player presses a key
+
+// eventListener keyPress
+
+// Math.floor(x)
