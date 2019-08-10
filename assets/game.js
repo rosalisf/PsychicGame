c0887f556086c3051de52348e470e931b51cbf33
Rosalis Freebairn <rfreeb@artic.edu>
2019-08-10 12:55:11 -0500

    page will refresh every time player wins and every 8 wrong guesses. added for span tags for the text in html.

--- assets/game.js
+++ assets/game.js
@@ -24,62 +24,6 @@
 //     delay: 1000
 //   });
 
-//  $(document).ready(function() {
-//         Here we are provided an initial array of letters.
-//         Use this array to set the alphabet that the computer and the player will guess.
-//         constters = [
-//           "A",
-//           "B",
-//           "C",
-//           "D",
-//           "E",
-//           "F",
-//           "G",
-//           "H",
-//           "I",
-//           "J",
-//           "K",
-//           "L",
-//           "M",
-//           "N",
-//           "O",
-//           "P",
-//           "Q",
-//           "R",
-//           "S",
-//           "T",
-//           "U",
-//           "V",
-//           "W",
-//           "X",
-//           "Y",
-//           "Z",
-//         ];
-
-// Creating an array of letters.
-//  const letters = ["A", "B", "C", "D"...];
-
-// Looping through each item in the array and logging a message to the console.
-
-//  for (let i = 0; i < letters.length; i++) {
-//    console.log(letters);
-//  }
-
-// Create the code that will prompt the computer to guess any of the 26 letters after player presses a key
-
-// list letters in array
-
-// eventListener keyPress
-
-// Math.floor(x)
-
-// randomly choose letter (computer)
-
-// listen to keydown
-
-// check keydown key against the computer
-
-// store wins and losses
 const letter = [
   "a",
   "b",
@@ -111,7 +55,7 @@ const letter = [
 
 let win = 0;
 let lose = 0;
-let wrongGuesses = 0;
+let wrongGuesses = 8;
 
 let computerGuess = getLetter();
 
@@ -121,9 +65,13 @@ document.addEventListener("keyup", function(event) {
   // console.log(computerGuess());
   if (playerGuess === computerGuess) {
     win++;
+    wrongGuesses = 8;
     console.log("win: ", win);
+  } else if (wrongGuesses === 0) {
+    lose++;
+    wrongGuesses = 8;
   } else {
-    wrongGuesses++;
+    wrongGuesses--;
     console.log("lose: ", lose);
   }
 });
@@ -131,10 +79,6 @@ document.addEventListener("keyup", function(event) {
 function getLetter() {
   return letter[Math.floor(Math.random() * Math.floor(letter.length - 1))];
 }
-
-// 8 guesses are allowed.
-// losses are counted when 8 guesses are incorrect and wins are counted anytime a guess is correct before guesses run out.
-// allow to refresh.
 
 // update dom for user
 // console.log(document.getElementById("wins"));
