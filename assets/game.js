fbf3eae6d3c3242acc60a8901b8fa6b2821f8c8b
Rosalis Freebairn <rfreeb@artic.edu>
2019-08-10 12:41:50 -0500

    When a key is pressed losses and wins will be logged in the console if the player presses a key the computer guesses.

--- assets/game.js
+++ assets/game.js
@@ -67,6 +67,92 @@
 
 // Create the code that will prompt the computer to guess any of the 26 letters after player presses a key
 
+// list letters in array
+
 // eventListener keyPress
 
 // Math.floor(x)
+
+// randomly choose letter (computer)
+
+// listen to keydown
+
+// check keydown key against the computer
+
+// store wins and losses
+const letter = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+let win = 0;
+let lose = 0;
+let wrongGuesses = 0;
+
+let computerGuess = getLetter();
+
+document.addEventListener("keyup", function(event) {
+  const playerGuess = event.key;
+  // console.log(playerGuess);
+  // console.log(computerGuess());
+  if (playerGuess === computerGuess) {
+    win++;
+    console.log("win: ", win);
+  } else {
+    wrongGuesses++;
+    console.log("lose: ", lose);
+  }
+});
+// computer will guess a letter immediately after player guesses.
+function getLetter() {
+  return letter[Math.floor(Math.random() * Math.floor(letter.length - 1))];
+}
+
+// 8 guesses are allowed.
+// losses are counted when 8 guesses are incorrect and wins are counted anytime a guess is correct before guesses run out.
+// allow to refresh.
+
+// update dom for user
+// console.log(document.getElementById("wins"));
+// document.getElementById("wins").innerHTML = wins;
+
+// update dom for user
+
+// document.addEventListener("keydown", function(event) {
+//   console.log(event.key);
+//   if (computerLetter === event.key) {
+//     alert("Hey buddy, you win!");
+//     // increment wins
+//     wins++;
+//     // update dom for user
+//     document.getElementById("wins").innerHTML = wins;
+//   } else {
+//     alert("Hey pa, you loose!");
+//     // increment losses
+//     // update dom for user
+//   }
+// });
